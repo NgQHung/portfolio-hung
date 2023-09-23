3c0eebe6b3b29f7b5274a0f6f7b47bc6f14cbaf8
NgQHung <hunghunghung273@gmail.com>
2023-09-23 17:12:52 +0200

    hotfixes

--- components/DarkLightButton.tsx
+++ components/DarkLightButton.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {MoonIcon, SunIcon} from './UI/Icons';
 import useThemeSwitcher from './hook/useThemeSwitcher';
 import {themeSwitcherActions} from '@/stores/ThemeSwitcher';
